2b977a0d0e6b8c34087fd1a0a57618cf8cf4e3b3
gokul-goklu <gokulshanmugam062@gmail.com>
2023-06-04 12:04:57 +0530

    first commit

---
+++ C10/Arithmetic.js
@@ -0,0 +1,40 @@
+let a = 10;
+let b = 20;
+
+// addition
+let add = a + b; // 30
+
+// subtract
+let subtract = a - b; // -10
+
+// divide
+let divide = a / b; // .5
+
+// multiply
+let multiply = a * b; // 200
+
+// remainder
+let remainder = a % b; // 10
+
+// power
+let power = a ** b; // 1(19)0
+
+let powerV2 = Math.pow(4, 3);
+
+console.log("Add: ", add);
+console.log("Subtract: ", subtract);
+console.log("divide: ", divide);
+console.log("multiply: ", multiply);
+console.log("remainder: ", remainder);
+console.log("power: ", power);
+console.log("powerV2: ", powerV2);
+console.log(
+  "Final: ",
+  add,
+  subtract,
+  divide,
+  multiply,
+  remainder,
+  power,
+  powerV2
+);
